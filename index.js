9303128208801b43212015a5e742fbf2e7490007
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-07-09 20:23:51 +0200

    edit

--- index.js
+++ index.js
@@ -7,8 +7,8 @@
 var before = 'LAST_MESSAGE_ID';
 clearMessages = function(){
 	const channel = window.location.href.split('/').pop();
-	const baseURL = `https://discordapp.com/api/channels/${channel}/messages`;
-	const headers = {"Authorization": "YOUR TOKEN HIER" };
+	const baseURL = `https://discord.com/api/v6/channels/${channel}/messages`;
+	const headers = {"Authorization": "YOUR_DISCORD_TOKEN"};
 
 	let clock = 0;
 	let interval = 500;
@@ -22,10 +22,11 @@ clearMessages = function(){
 	fetch(baseURL + '?before=' + before + '&limit=100', {headers})
 		.then(resp => resp.json())
 		.then(messages => {
-		return Promise.all(messages.map((message) => {
-			before = message.id;
-			return delay(clock += interval).then(() => fetch(`${baseURL}/${message.id}`, {headers, method: 'DELETE'}));
-		}));
+			console.log(messages)
+			return Promise.all(messages.map((message) => {
+				before = message.id;
+				return delay(clock += interval).then(() => fetch(`${baseURL}/${message.id}`, {headers, method: 'DELETE'}));
+			}));
 	}).then(() => clearMessages());
 }
 clearMessages();
